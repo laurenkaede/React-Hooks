e673f191e11c59832d81c631218c9e07d8e3fc1f
Lauren Moor <laurenkaede@gmail.com>
2020-04-03 15:41:20 +0100

    Updates

---
+++ precache-manifest.6ace40a2f3f9fe97da30f8ba87b2e460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62170b5fa1622be12df9f38a2166d0f",
+    "url": "/React-Hooks/index.html"
+  },
+  {
+    "revision": "049d98e7ae9d1b86e661",
+    "url": "/React-Hooks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d7804d4bb07dbb749a5",
+    "url": "/React-Hooks/static/js/2.72058d14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Hooks/static/js/2.72058d14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049d98e7ae9d1b86e661",
+    "url": "/React-Hooks/static/js/main.c06fd609.chunk.js"
+  },
+  {
+    "revision": "f15e803e6d8a42fc0145",
+    "url": "/React-Hooks/static/js/runtime-main.7ca5d268.js"
+  }
+]);
